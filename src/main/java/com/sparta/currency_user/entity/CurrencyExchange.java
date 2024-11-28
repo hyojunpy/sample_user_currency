a56ed58ee6aab721858faccb19c685faddb18746
Hyojun Pyeon <vusgywns5946@gmail.com>
2024-11-28 15:16:28 +0900

    feat : Lv 1. 고객(User)과 통화(Currency) 복잡한 연관관계 설정 완료

---
+++ src/main/java/com/sparta/currency_user/entity/CurrencyExchange.java
@@ -0,0 +1,31 @@
+package com.sparta.currency_user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyExchange extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "to_currency_id")
+    private Currency currency;
+
+    private Long amountInKrw;
+
+    private Double amountAfterExchange;
+
+    private String status = "OK";
+
+}
